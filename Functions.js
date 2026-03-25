3dd46ef4afa990848d7c9a7020c1d24c90651920
Ahmad M.Amin <ahmadmuhammadamin327@gmail.com>
2026-03-25 05:51:24 -0700

    New Line Add

--- Functions.js
+++ Functions.js
@@ -71,3 +71,68 @@ function countVowels(str) {
 
     console.log(count);
 }
+// For Each Function Use
+let arr = [1, 2, 3, 4, 5];
+arr.forEach(function printval(val) {
+    console.log(val);
+});
+// For Each Function Use Second Way
+{
+let arr = ["Pakistan", "Peshawar", "Islamabad", "Lahore"];
+arr.forEach((val, idx, arr) => {
+    console.log(val.toUpperCase(), idx, arr);
+});
+}
+// Practice Question 01
+let nums = [1, 4, 6, 9, 12];
+nums.forEach((num) => {
+    console.log(num * num)
+});
+// Practice Question 01 Second Way
+{
+let nums = [10, 8, 5];
+let calcSquare =(num) => {
+    console.log(num * num);
+};
+nums.forEach(calcSquare);
+}
+// Function Use for Even Number
+{
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let evenArr = arr.filter((val) => {
+    return val % 2 === 0;
+});
+console.log(evenArr);
+}
+// Function Use for Odd Number
+{
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let evenArr = arr.filter((val) => {
+    return val % 2 !== 0;
+});
+console.log(evenArr);
+}
+// Function use Reduced
+{
+let arr = [1, 2, 3, 4,];
+const output = arr.reduce((res, curr) => {
+    return res + curr;
+});
+console.log(output);
+}
+// Function use Reduced Larger number
+{
+let arr = [1, 2, 3, 4];
+const output = arr.reduce((res, curr) => {
+    return res > curr ? res : curr;
+});
+console.log(output);
+}
+// Practice Number 01 for Apna College
+{
+let marks = [97, 92, 77, 34, 94, 23];
+let toppers = marks.filter((val) => {
+    return val > 90;
+});
+console.log(toppers);
+}
